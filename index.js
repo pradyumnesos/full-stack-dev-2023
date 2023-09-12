5045030fdd278e452a5e9e4201997a78a6147e83
pradyumnesos <pradyumnaojha856@gmail.com>
2023-09-12 17:32:20 +0530

    Solved assgn 4 challenge

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const products = data.products;
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
-
-  if(req.url.startsWith('/product')){
-    const id = req.url.split('/')[2]
+   
+  if(req.url.startsWith('/product?')){
+    const id = req.url.split('=')[1]
     const product = products.find(p=>p.id===(+id))
     console.log(product)
     res.setHeader('Content-Type', 'text/html');
@@ -20,6 +20,18 @@ const server = http.createServer((req, res) => {
           res.end(modifiedIndex);
           return;
   }
+  // if(req.url.startsWith('/product')){
+  //   const id = req.url.split('/')[2]
+  //   const product = products.find(p=>p.id===(+id))
+  //   console.log(product)
+  //   res.setHeader('Content-Type', 'text/html');
+  //         let modifiedIndex = index.replace('**title**', product.title)
+  //         .replace('**url**', product.thumbnail)
+  //         .replace('**price**', product.price)
+  //         .replace('**rating**', product.rating)
+  //         res.end(modifiedIndex);
+  //         return;
+  // }
 //   '/product':
 //       res.setHeader('Content-Type', 'text/html');
 //       let modifiedIndex = index.replace('**title**', product.title)
